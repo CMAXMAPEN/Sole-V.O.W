5c4139d724a714d67fba7a9fd785147799763c7c
rhardock <rhardock@gmail.com>
2022-11-10 08:43:29 -0500

    Added some more assets and attempt at a scene with the player walking
    Assets were from:
    https://assetstore.unity.com/packages/2d/environments/free-platform-game-assets-85838
    (not sure about using them yet)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float minX = -50f, minY=-50f, maxX=100f, maxY=100f;
+    public float jumpHeight = 0.5f;
+
+    private Rigidbody2D rBody;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        rBody.freezeRotation = true;
+    }
+    private void FixedUpdate()
+    {
+        // initial empty state, stay here until finish falling
+        if (animator.GetInteger("AnimState") == 0)
+            return;
+
+        float horiz, vert;
+
+        horiz = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+
+        // var absVelx = Mathf.Abs(rBody.velocity.x);
+        // var absVely = Mathf.Abs(rBody.velocity.y);
+
+        if (Mathf.Abs(horiz) > .1f)
+        {
+            transform.localScale = new Vector3(horiz > 0 ? 1 : -1, 1, 1);
+            animator.SetInteger("AnimState", 2);
+            Vector2 newVelocity = new Vector2(horiz, 0f);
+            rBody.velocity = newVelocity * speed;
+        }
+        else
+        {
+            animator.SetInteger("AnimState", 1);
+        }
+
+        /*
+        float newX, newY;
+        newX = Mathf.Clamp(rBody.position.x, minX, maxX);
+        newY = Mathf.Clamp(rBody.position.y, minY, maxY);
+
+        rBody.position = new Vector2(newX, newY);
+        */
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (animator.GetInteger("AnimState") == 0)
+        {
+            animator.SetInteger("AnimState", 1);
+        }
+        print("Collision Hit");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
