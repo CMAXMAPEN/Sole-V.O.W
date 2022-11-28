6114f726a2640c4db61da3086255688408ae679c
CMXN <acalde15@my.centennialcollege.ca>
2022-11-28 16:52:05 -0500

    Added audio manager
    Added an audio manager and script for future one-off audio clips such as single shots, healing, jumping/landing, etc. 
    Also gave the player some footsteps for his running animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,18 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController2D controller;
     public Animator animator;
 
+
     float horizontalMove = 0f;
     public float runSpd = 40f;
     bool jump = false;
-    
+    int vert = 1;
      
+
+    [Header ("SFX")]
+    [SerializeField]public AudioSource stepSound;
+    [SerializeField] private AudioClip jumpSound;
+
+    
 
 
     // Update is called once per frame
@@ -22,23 +29,37 @@ public class PlayerMovement : MonoBehaviour
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpd;  //take the horizontal input and multiply it by player spd
 
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove)); //if Speed is positive then it switches to running anim. This sees if it is moving
+        
+       if(Input.GetButton("Horizontal") == true && vert ==0)
+       {
+        stepSound.enabled = true;
+       }
+       
+        else
+        {
+            stepSound.enabled = false;
+        }
+
 
         if(Input.GetButtonDown("Jump"))
         {
             jump = true;
             animator.SetBool("IsJumping", true);
+            vert = 1;
         }
         
     }
 
         public void OnLanding(){
-            
+            vert = 0;
             animator.SetBool("IsJumping", false);
         }
 
     void FixedUpdate ()
     {
+        
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        
         jump = false;
        
        
