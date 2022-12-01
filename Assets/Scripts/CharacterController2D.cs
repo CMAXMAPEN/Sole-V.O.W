3b7bc8b1ecc2fe58bf7622ca35d208d44a8ff133
rhardock <rhardock@gmail.com>
2022-12-01 01:22:11 -0500

    Added camera tracking using the Cinemachine package

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -48,7 +48,8 @@ public class CharacterController2D : MonoBehaviour
 
 		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
-		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position,
+                                                            k_GroundedRadius, m_WhatIsGround);
 		for (int i = 0; i < colliders.Length; i++)
 		{
 			if (colliders[i].gameObject != gameObject)
@@ -92,7 +93,8 @@ public class CharacterController2D : MonoBehaviour
 				// Disable one of the colliders when crouching
 				if (m_CrouchDisableCollider != null)
 					m_CrouchDisableCollider.enabled = false;
-			} else
+			}
+            else
 			{
 				// Enable the collider when not crouching
 				if (m_CrouchDisableCollider != null)
@@ -108,7 +110,8 @@ public class CharacterController2D : MonoBehaviour
 			// Move the character by finding the target velocity
 			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
 			// And then smoothing it out and applying it to the character
-			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity,
+                                                        ref m_Velocity, m_MovementSmoothing);
 
 			// If the input is moving the player right and the player is facing left...
 			if (move > 0 && !m_FacingRight)
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,42 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
     public CharacterController2D controller;
     public Animator animator;
-
 
     float horizontalMove = 0f;
     public float runSpd = 40f;
     bool jump = false;
     int vert = 1;
-     
 
     [Header ("SFX")]
     [SerializeField]public AudioSource stepSound;
     [SerializeField] private AudioClip jumpSound;
 
-    
-
-
     // Update is called once per frame
     void Update()
     {
-        
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpd;  //take the horizontal input and multiply it by player spd
+        // Take the horizontal input and multiply it by player speed
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpd;
 
-        animator.SetFloat("Speed", Mathf.Abs(horizontalMove)); //if Speed is positive then it switches to running anim. This sees if it is moving
+        // If Speed is positive then it switches to running anim.
+        // This sees if it is moving.
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
         
-       if(Input.GetButton("Horizontal") == true && vert ==0)
-       {
-        stepSound.enabled = true;
-       }
-       
+        if(Input.GetButton("Horizontal") == true && vert ==0)
+        {
+            stepSound.enabled = true;
+        }
         else
         {
             stepSound.enabled = false;
         }
-
 
         if(Input.GetButtonDown("Jump"))
         {
@@ -47,21 +41,16 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("IsJumping", true);
             vert = 1;
         }
-        
     }
 
-        public void OnLanding(){
-            vert = 0;
-            animator.SetBool("IsJumping", false);
-        }
+    public void OnLanding(){
+        vert = 0;
+        animator.SetBool("IsJumping", false);
+    }
 
     void FixedUpdate ()
     {
-        
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
-        
         jump = false;
-       
-       
     }
 }
