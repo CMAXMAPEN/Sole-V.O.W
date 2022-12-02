dc4f86855b47ed8419ab7589b6acfd776db1e0e9
rhardock <rhardock@gmail.com>
2022-12-02 05:22:59 -0500

    Added a count-down timer, game over and restart.
    The timer is in seconds.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    [Header("UI Objects")]
+    public TextMeshProUGUI scoreText;
+    public GameObject gameOverTextObj;
+    public GameObject restartTextObj;
+    public TextMeshProUGUI timerText;
+
+    private int score = 0;
+    private bool gameover = false;
+    private bool restart = false;
+    public float timeLeft;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScoreText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if we need to restart
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                // Restart our game.
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+
+        if (!gameover)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft >= 0)
+            {
+                UpdateTimer(timeLeft);
+            }
+            else 
+            {
+                GameOver();
+            }
+        }
+
+    }
+
+    void UpdateTimer(float currentTime)
+    {
+        timerText.text = string.Format($"{currentTime:00}");
+    }
+
+    public void AddToScore(int scoreValueToAdd)
+    {
+        score += scoreValueToAdd;
+        // Debug.Log("Score: " + score);
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        // What happens whe my game is over?
+        animator.SetBool("IsDead", true);
+        
+        gameover = true;
+        restart = true;
+        gameOverTextObj.SetActive(true);
+        restartTextObj.SetActive(true);
+    }
+}
